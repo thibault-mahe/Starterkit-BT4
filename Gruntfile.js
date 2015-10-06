0c6f21d143e04b53a980bc5e659e984eecfdba0e
tibomahe <thibault.mahe@gmail.com>
2015-10-06 17:55:30 +0200

    Update SBT4

---
+++ Gruntfile.js
@@ -0,0 +1,179 @@
+module.exports = function(grunt) {
+
+	grunt.initConfig({
+
+		pkg: grunt.file.readJSON('package.json'),
+
+
+		jshint: {
+			files: ['Gruntfile.js', 'js/main.js'],
+			options: {
+				globals: {
+					jQuery: true,
+					console: true,
+					module: true
+				}
+			}
+		},
+
+		autoprefixer: {
+	        dist: {
+	            options: {
+	              browsers: ['last 2 version', 'ie 9', 'Firefox > 20', 'Safari > 5'],
+	              flatten: true
+	            },
+	            files: {
+	                'css/main.css': 'css/main.css'
+	            }
+	        }
+	    },
+
+	    csso: {
+	          compress: {
+	              options: {
+	                  report: 'min'
+	              },
+	              files: {
+	                  'css/main.min.css': 'css/main.css'
+	              }
+	          }
+	     },
+
+	    imagemin: {
+	      dynamic: {
+	        files: [{
+	            expand: true,
+	            cwd: 'im/',
+	            src: ['**/*.{png,jpg,gif}'],
+	            dest: 'im/'
+	        }]
+	      }
+	    },
+
+	    sass: {
+	      options: {
+	        sourceMap: false
+	      },
+	      dist: {
+	        files: {
+	          'css/main.css': 'scss/main.scss'
+	        }
+	      }
+	    },
+
+		concat: {
+			main: {
+				src: ['vendor/jquery/dist/jquery.js',
+					'vendor/bootstrap/dist/js/bootstrap.js',
+					'js/*.js'],
+				dest: 'dist/<%= pkg.name %>-<%= pkg.version %>.js'
+			},
+			ieSupport: {
+				src: ['vendor/html5shiv/dist/html5shiv.js',
+					'vendor/respond/dest/respond.js'],
+				dest: 'dist/<%= pkg.name %>-<%= pkg.version %>-ie-support.js'
+			}
+		},
+
+		uglify : {
+			js: {
+				files: {
+					'dist/<%= pkg.name %>-<%= pkg.version %>.js' : [ 'dist/<%= pkg.name %>-<%= pkg.version %>.js' ],
+					'dist/<%= pkg.name %>-<%= pkg.version %>-ie-support.js' : ['dist/<%= pkg.name %>-<%= pkg.version %>-ie-support.js']
+				}
+			}
+		},
+
+		connect: {
+			localhost: {
+				options: {
+					port: 9001,
+					open: {
+						target: 'http://localhost:9001/'
+					},
+					keepalive: false,
+					base: [''],
+					livereload: false,
+					hostname: 'localhost',
+				}
+			}
+		},
+
+		delta: {
+			options: {
+				livereload: true,
+			},
+			gruntfile: {
+		        files: 'Gruntfile.js',
+		        tasks: [ 'jshint' ],
+		        options: {
+		          livereload: false
+		        }
+		    },
+			sass: {
+				files: 'scss/*.scss',
+				tasks: ['sass', 'autoprefixer', 'csso'],
+			},
+			script: {
+				files: 'js/*.js',
+				tasks: ['jshint', 'concat']
+			},
+			html: {
+				files: ['index.html'],
+				tasks: ['sass', 'autoprefixer', 'csso', 'concat']
+			},
+			images: {
+				files: ['im/**/*.{png,jpg,gif}'],
+				tasks: ['imagemin']
+			}
+		},
+
+		//When the critical css is generated, copy and paste it
+		//to insert it in the adequate view.
+		criticalcss: {
+			home: {
+				options:  {
+					outputfile : 'css/critical/critical-home.css',
+					filename : 'css/main.css',
+					url : 'http://localhost:9001',
+					width: 1200,
+                	height: 900
+				}
+			}
+			// view: {
+			// 	options:  {
+			// 		outputfile : 'css/critical/critical-viewName.css',
+			// 		filename : 'css/main.css',
+			// 		url : 'path/to/view.html'
+			// 	}
+			// }
+		}
+
+	});
+
+	grunt.loadNpmTasks('grunt-contrib-jshint');
+	grunt.loadNpmTasks('grunt-autoprefixer');
+	grunt.loadNpmTasks('grunt-contrib-concat');
+	grunt.loadNpmTasks('grunt-contrib-imagemin');
+	grunt.loadNpmTasks('grunt-contrib-uglify');
+	grunt.loadNpmTasks('grunt-contrib-watch');
+	grunt.loadNpmTasks('grunt-csso');
+	grunt.loadNpmTasks('grunt-sass');
+	grunt.loadNpmTasks('grunt-contrib-connect');
+	grunt.loadNpmTasks('grunt-criticalcss');
+
+	grunt.registerTask('images', ['imagemin']);
+	grunt.renameTask( 'watch', 'delta' );
+	grunt.registerTask('default', [
+		'jshint',
+		'sass',
+		'autoprefixer',
+		'csso',
+		'concat',
+		'imagemin',
+		'connect:localhost',
+		'criticalcss',
+		'delta'
+	]);
+
+};
---
+++ README.md
@@ -0,0 +1,56 @@
+Starterkit-BT4
+===========
+
+A starter kit for your web project based on Bootstrap 4, with grunt and bower
+## Assets
+
+- **Bootstrap 4 (alpha version, [see documentation](http://v4-alpha.getbootstrap.com/getting-started/introduction/))**
+- **SASS**
+- **Respond.JS**
+- **html5shiv**
+- **Grunt**
+- **Bower**
+
+## License
+
+Starterkit-BT4 is released under the [MIT License](COPYING).
+
+## Getting Started
+
+This project requires [Grunt](http://gruntjs.com/) and [Bower](http://bower.io/).
+
+From the project's directory execute :
+
+```
+$ npm -g install grunt-cli bower
+$ npm install
+$ bower install
+```
+
+Run project with Grunt.
+
+```
+$ grunt
+```
+
+## Bootstrap 4 and bower
+
+If you are having difficulty installing BT4 with bower, since it is still an alpha version, use :
+
+```
+$ bower install bootstrap#v4.0.0-alpha
+```
+
+## Critical CSS
+
+Starterkit-BT4 (SBT4 in short) uses the grunt contrib for [criticalcss](https://github.com/filamentgroup/grunt-criticalcss). When you launch grunt, it generates critical css files in css/critical/.. that you can copy, paste and inline in the head of your html.
+
+## Switch from LESS to SASS (work in progress)
+
+If you used to use LESS, you might be interested in these readings :
+- the SASS website : [http://sass-lang.com/](http://sass-lang.com/)
+- some SASS resources : [http://thesassway.com/](http://thesassway.com/)
+- how to override the Bootstrap variables (the variables are interpreted differently) : [https://github.com/RailsApps/rails-bootstrap/issues/12](https://github.com/RailsApps/rails-bootstrap/issues/12)
+- using REM instead of px is very awesome for a lot of reasons I won't enumerate here. To facilitate the switch to REM unit, you can use the [calc-rem() function](https://github.com/taupecat/sass-responsive/blob/master/_responsive.scss#L146) defined in the scss/custom-variables.scss file, unless you ask grunt [to manage it for you](https://www.npmjs.com/package/grunt-px-to-rem).
+
+
